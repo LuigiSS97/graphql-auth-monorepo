62b09219e5a5f25f7db89e844e734fa30e8d4f8a
Luigi Souza <luigisouzasc@gmail.com>
2023-11-28 17:14:53 -0300

    mutations login,logout and signup

---
+++ server/schema/mutations.js
@@ -0,0 +1,41 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
+const UserType = require("./types/user_type");
+const AuthService = require("../services/auth");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, args, req) {
+        return AuthService.signup({ ...args, req });
+      },
+    },
+    logout: {
+      type: UserType,
+
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      },
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, args, req) {
+        return AuthService.login({ ...args, req });
+      },
+    },
+  },
+});
+
+module.exports = mutation;
