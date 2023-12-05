0d19c7511420fac8d593e52ba31c096e951a9c85
Luigi Souza <luigisouzasc@gmail.com>
2023-12-05 19:07:56 -0300

    redirecting flow fix

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,26 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+
 import { graphql } from "react-apollo";
+
 import CurrentUser from "../queries/CurrentUser";
-import { compose } from "recompose";
-import { hashHistory } from "react-router";
 
 function RequireAuth(WrappedComponent) {
-  function AuthComponent(props) {
-    const prevProps = useRef(props);
+  return graphql(CurrentUser)((props) => {
+    const { user, loading } = props.data;
 
-    useEffect(() => {
-      if (prevProps !== props) {
-        if (!props.data.loading && !props.data.user) {
-          hashHistory.push("/login");
-        }
-      }
-      prevProps.current = props;
-    });
-
-    return <WrappedComponent {...props} />;
-  }
-
-  return compose(graphql(CurrentUser))(AuthComponent);
+    if (user && !loading) {
+      return <WrappedComponent {...props} />;
+    }
+    return (
+      <div>
+        <h3>You must be logged in to view this page</h3>
+      </div>
+    );
+  });
 }
 
 export default RequireAuth;
